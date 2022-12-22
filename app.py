6ca6d978df9d794aee0b56bd5b0bc8c273b01a73
Hamzah Jamad <hamzah@digiheritage.com.my>
2022-12-22 16:03:17 +0800

    feat: add file checker and exclude already created pdf

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
-from helpers import pdf_title_generator, pdf_file_generator
+from helpers import pdf_title_generator, pdf_file_generator, move_to_last_chapters
 
 load_dotenv()
 
+LAST_CHAPTER = os.getenv('LAST_CHAPTER')
 MANGA_DIRECTORY = os.getenv('MANGA_DIRECTORY')
 MANGA_TITLE = os.getenv('MANGA_TITLE')
 IS_USING_HUMANIZED_CHAPTER_TITLE = os.getenv('IS_USING_HUMANIZED_CHAPTER_TITLE') \
@@ -11,7 +12,12 @@ IS_USING_HUMANIZED_CHAPTER_TITLE = os.getenv('IS_USING_HUMANIZED_CHAPTER_TITLE')
 
 SOURCE_PATH = MANGA_DIRECTORY + MANGA_TITLE + "/"
 
-for directory_details in os.walk(SOURCE_PATH):
+all_items = list(os.walk(SOURCE_PATH))
+
+if LAST_CHAPTER  != "":
+    all_items = move_to_last_chapters(all_items, LAST_CHAPTER)
+
+for directory_details in all_items:
     chapter_path = directory_details[0] + "/"
     chapter_directory_name = chapter_path.split("/")[-2:][0]
     chapter_title = pdf_title_generator(
--- helpers.py
+++ helpers.py
@@ -2,6 +2,15 @@ import os
 import re
 from PIL import Image
 
+def get_all_chapters_path(chapters):
+    return [item[0].split("/")[-1:][0] for item in chapters]
+
+def move_to_last_chapters(all_items, last_chapter):
+    all_chapters = get_all_chapters_path(all_items)
+    index_last_chapter = all_chapters.index(last_chapter)
+
+    return all_items[index_last_chapter + 1:]
+
 def chapter_directory_to_human_language_transformer(chapter_directory):
     # get the Ch.XXX pattern
 
@@ -36,12 +45,17 @@ def pdf_title_generator(manga_title, chapter_directory, is_using_humanized_chapt
 
     return result
 
+def is_image_file(path):
+    return path.endswith('.jpg') or \
+           path.endswith('.jpeg') or \
+           path.endswith('.png')
+
 def pdf_file_generator(source_path, chapter_title):
     output_file = "output/" + chapter_title
     result = []
 
     for path in os.listdir(source_path):
-        if os.path.isfile(os.path.join(source_path, path)):
+        if os.path.isfile(os.path.join(source_path, path)) and is_image_file(path):
             image = Image.open(source_path + path) \
                          .convert('RGB')
             result.append(image)
